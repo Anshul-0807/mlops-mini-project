a7fc2a25451c67e405e7ea872df7f52f2a4d09f5
Anshul patle <patleanshul920@gmail.com>
2024-12-01 23:34:09 +0530

    2nd exp done bow_vs_ tfidf

--- notebooks/dagshub_setup.py
+++ notebooks/dagshub_setup.py
@@ -3,6 +3,7 @@ import mlflow
 
 dagshub.init(repo_owner='Anshul-0807', repo_name='mlops-mini-project', mlflow=True)
 
+
 mlflow.set_tracking_uri("https://dagshub.com/Anshul-0807/mlops-mini-project.mlflow")
 with mlflow.start_run():
   mlflow.log_param('parameter name', 'value')
